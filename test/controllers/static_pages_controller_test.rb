8bca26b7656e48381e24c1dc05d5e0ae4c2923fc
Shocker55 <bialetti360@gmail.com>
2022-10-15 07:37:00 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
@@ -18,10 +23,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Help | #{@base_title}"
   end
 
-  test "should get aboud" do
+  test "should get about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
